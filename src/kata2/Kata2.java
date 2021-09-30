adce6393b4f49ec098933fd69fe61d7ebd757820
equipo <equipo@DESKTOP-S4N862J>
2021-09-30 10:23:50 +0100

    Se ha cambiado el tipo de data a array de Strings

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,15 +6,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer[] data = new Integer[15];
-        for (int i=0; i<data.length; i++) {
-           data[i] = (int) ((Math.random())*5);
-        }
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
         
-        for (int key : histogr.keySet()) {
+        for (String key : histogr.keySet()) {
             System.out.println(key + "==>" + histogr.get(key));
         }
     }
